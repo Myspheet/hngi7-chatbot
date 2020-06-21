a9e72b787e536197c4d75dc162778adbe9b5793a
Myspheet <myspheet@gmail.com>
2020-06-21 03:01:38 +0100

    Replaced socket.io with fetch api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const server = require("http").Server(app);
+const session = require("express-session");
 const io = require("socket.io")(server);
 const port = process.env.PORT || 7000;
 const WitService = require("./service/WitService");
@@ -10,6 +11,16 @@ const ConversationService = require("./service/ConversationService");
 require("dotenv").config();
 
 // middleware
+app.use(express.json());
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
+
 app.use(express.static("public"));
 
 const witService = new WitService(process.env.WIT_ACCESS_TOKEN);
@@ -19,33 +30,47 @@ server.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
+  req.session.context = {};
   res.sendFile(__dirname + "/public/index.html");
 });
-const context = {};
-io.on("connection", (socket) => {
-  socket.context = context;
-  io.emit("message", {
-    message:
-      "Hello, I'm Resi and I can help you book a reservation at the Continental Diner!",
-  });
-  socket.on("message", async (data) => {
-    io.emit("message", { message: data.chatMsg, user: data.user });
-    const context = await ConversationService.run(
-      witService,
-      data.chatMsg,
-      socket.context
-    );
-    console.log(socket.context.conversation.complete);
-    if (!socket.context.conversation.complete) {
-      io.emit("message", {
-        message: context.conversation.followUp,
-        user: "bot",
-      });
-    } else {
-      io.emit("message", {
-        message: context.conversation.followUp,
-        user: "bot",
-      });
-    }
-  });
+
+app.post("/", async (req, res) => {
+  // res.sendFile(__dirname + "/public/index.html");
+
+  if (!req.session.context) {
+    req.session.context = {};
+  }
+  const context = await ConversationService.run(
+    witService,
+    req.body.message,
+    req.session.context
+  );
+  res.json({ message: context.conversation.followUp });
 });
+// io.on("connection", (socket) => {
+//   socket.context = context;
+//   io.emit("message", {
+//     message:
+//       "Hello, I'm Resi and I can help you book a reservation at the Continental Diner!",
+//   });
+//   socket.on("message", async (data) => {
+//     io.emit("message", { message: data.chatMsg, user: data.user });
+//     const context = await ConversationService.run(
+//       witService,
+//       data.chatMsg,
+//       socket.context
+//     );
+//     console.log(socket.context.conversation.complete);
+//     if (!socket.context.conversation.complete) {
+//       io.emit("message", {
+//         message: context.conversation.followUp,
+//         user: "bot",
+//       });
+//     } else {
+//       io.emit("message", {
+//         message: context.conversation.followUp,
+//         user: "bot",
+//       });
+//     }
+//   });
+// });
